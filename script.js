92c625e0fed2c8cb9060cd50e592891d77c2e4a3
shravan1101 <shravanbhise248@gmail.com>
2025-06-24 21:57:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+console.log("h");
+
+var a = 5;
+var b = 6;
+var c = 5;
+
+console.log(a+c+b+1);
+console.log(typeof a , typeof b , typeof c);
+
+// {
+//     let a =100;
+//     console.log(a);
+//     console.log(typeof a);
+
+// }
+
+console.log("next part");
+let x = "shravan ";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q =undefined;
+let r = null;
+
+console.log(x,y,z,p,q,r)
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r)
+
+let o = {
+ "name" : "shravan bhsie ",
+ "jod code" : 123,
+ "chikana" :true
+}
+
+console.log(o);
+o.money="100000crores";
+console.log(o);
+o.money="bnak corrept";
+console.log(o);
